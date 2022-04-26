006b3eb770e9390a01b743b8da61bb6d929668d1
S-404 <sudarev.andrey.i@gmail.com>
2022-04-26 13:36:43 +0300

    add validations.ts

---
+++ src/helpers/validations.ts
@@ -0,0 +1,31 @@
+export const isValidEmail = (email: string) => {
+    return !!email
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+}
+
+export const isValidString = (text: string) => {
+    return !!text.match(
+        /^[а-яА-Яa-zA-Z\s]+/
+    )
+}
+
+export const isValidPhone = (text: string) => {
+    return !!text.match(
+        /^[+\-]?[0-9]{0,3}?[-\s.]?[(]?[0-9]{1,3}[)]?[-\s.]?[-\s./0-9]*(x|ext\.?)?[-\s./0-9]{0,10}$/
+    )
+}
+
+export const isValidUsername = (text: string) => {
+    return !!text.match(
+        /^[a-zA-Z0-9_-]*$/
+    )
+}
+
+export const isValidPassword = (text: string) => {
+    return !!text.match(
+        /^(?=.*\d)[a-zA-Z0-9]{4,}$/
+    )
+}
