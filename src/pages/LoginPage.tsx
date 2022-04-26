0e74b7f3e830858666cf1e0522225185c271248f
S-404 <sudarev.andrey.i@gmail.com>
2022-04-26 17:04:44 +0300

    update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,60 +1,27 @@
-import React, {FC, useState} from 'react'
-import {useTypedSelector} from '../hooks/useTypedSelector'
-import {useActions} from '../hooks/useActions'
-import {Link} from 'react-router-dom'
+import React, {FC} from 'react'
+import {Container,Box,Typography} from '@mui/material'
+import LoginForm from '../components/loginForm/LoginForm'
 
 const LoginPage: FC = () => {
-    const [username, setUsername] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-    const {loading, error} = useTypedSelector(state => state.auth)
-    const {login} = useActions()
 
-    const handlerUsernameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(e.target.value)
-    }
-
-    const handlerPasswordInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value)
-    }
-
-    const handlerLoginButton = () => {
-        if (username && password) login(username, password)
-    }
     return (
-        <div>
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography component="h1" variant="h5">
+                    Sign in to BLAHAJ
+                </Typography>
 
-            <h2>Sign in to BLAHAJ</h2>
+                <LoginForm/>
 
-            <div>
-
-                <h3>Username</h3>
-                <input
-                    value={username}
-                    onChange={handlerUsernameInput}
-                />
-
-
-                <h3>Password</h3>
-                <input
-                    type={'password'}
-                    value={password}
-                    onChange={handlerPasswordInput}
-                />
-
-                {error ? <div>{error}</div> : null}
-                <button
-                    color="success"
-
-                    onClick={handlerLoginButton}
-                >
-                    {loading ? 'Signing in...' : 'Sign in'}
-                </button>
-                <div>
-                    <i> <Link to={'/registration'}>Create an account</Link></i>
-                </div>
-
-            </div>
-        </div>
+            </Box>
+        </Container>
     )
 }
 
