fcdce84f5e38db5d238eec6672ac9eb9e3a798d9
S-404 <sudarev.andrey.i@gmail.com>
2022-04-26 17:04:17 +0300

    update RegistrationPage.tsx

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,18 +1,30 @@
 import React, {FC} from 'react'
-import RegistrationInputs from '../components/registrationInputs/RegistrationInputs'
+import RegistrationForm from '../components/registrationForm/RegistrationForm'
+import {Container} from '@mui/material'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
 
 const RegistrationPage: FC = () => {
 
-
     return (
-        <div>
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography component="h1" variant="h5">
+                    Sign up to BLAHAJ
+                </Typography>
 
-            <h2>Sign up to BLAHAJ</h2>
+                <RegistrationForm/>
 
-            <div>
-                <RegistrationInputs/>
-            </div>
-        </div>
+            </Box>
+        </Container>
     )
 }
 
