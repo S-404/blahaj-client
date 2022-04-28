062da2d30767f9beb15a0aa37885f04c6198cd9e
S-404 <sudarev.andrey.i@gmail.com>
2022-04-28 16:15:58 +0300

    refactor RegistrationPage.tsx

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,26 +1,31 @@
 import React, {FC, useEffect} from 'react'
 import RegistrationForm from '../components/registrationForm/RegistrationForm'
-import {Container} from '@mui/material'
+import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import {useActions} from '../hooks/useActions'
 
 
 const RegistrationPage: FC = () => {
+
     const {setSelectedPage} = useActions()
+
     useEffect(()=>{
         setSelectedPage('Registration')
     },[])
+
+    const boxStyles = {
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    }
+
     return (
         <Container component="main" maxWidth="xs">
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
+
+            <Box sx={boxStyles}>
+
                 <Typography component="h1" variant="h5">
                     Sign up to BLAHAJ
                 </Typography>
@@ -28,6 +33,7 @@ const RegistrationPage: FC = () => {
                 <RegistrationForm/>
 
             </Box>
+
         </Container>
     )
 }
