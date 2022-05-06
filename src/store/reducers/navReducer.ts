d54f6a383fa86c07c0920e4302f41097d85e394e
S-404 <sudarev.andrey.i@gmail.com>
2022-05-06 14:26:00 +0300

    upd navReducer.ts - add 'selectedTeam'

--- src/store/reducers/navReducer.ts
+++ src/store/reducers/navReducer.ts
@@ -1,8 +1,10 @@
-import {NavAction, NavActionTypes, NavState} from "../types/navTypes"
+import {NavAction, NavActionTypes, NavState} from '../types/navTypes'
+import {ITeam} from '../../services/types/teamsResponse'
 
 const navState: NavState = {
     selectedPage: 'Home',
-    isOpenDrawer: false
+    isOpenDrawer: false,
+    selectedTeam: {} as ITeam,
 }
 
 export const navReducer = (state = navState, action: NavAction): NavState => {
@@ -11,6 +13,8 @@ export const navReducer = (state = navState, action: NavAction): NavState => {
             return {...state, selectedPage: action.value}
         case NavActionTypes.SET_IS_OPEN_DRAWER:
             return {...state, isOpenDrawer: action.value}
+        case NavActionTypes.SET_SELECTED_TEAM:
+            return {...state, selectedTeam: action.value}
         default:
             return state
     }
