536f255d42139450288ccfe755490a1cd92b73b2
S-404 <sudarev.andrey.i@gmail.com>
2022-05-06 19:45:02 +0300

    add actions createTask , removeTask

--- src/store/action-creators/todos/index.ts
+++ src/store/action-creators/todos/index.ts
@@ -1,6 +1,10 @@
 import {fetchTasks} from './fetchTasks'
+import {createTask} from './createTask'
+import {removeTask} from './removeTask'
 
 
 export const tasksActions = {
     fetchTasks,
+    createTask,
+    removeTask
 }
---
+++ src/store/action-creators/todos/removeTask.ts
@@ -0,0 +1,25 @@
+import {Dispatch} from 'redux'
+import axios from 'axios'
+import {TasksAction, TasksActionTypes} from '../../types/tasksTypes'
+import TasksService from '../../../services/tasksService'
+
+export const removeTask = (taskId: number) => {
+    return async (dispatch: Dispatch<TasksAction>) => {
+        try {
+            dispatch({type: TasksActionTypes.FETCH_TASKS})
+            await TasksService.removeTask(taskId)
+            dispatch({type: TasksActionTypes.REMOVE_TASK, value: taskId})
+        } catch (e) {
+            let errMsg
+            if (axios.isAxiosError(e)) {
+                errMsg = e?.response?.data?.message
+            }
+            let message = errMsg ? errMsg : 'connection failed'
+            dispatch({
+                type: TasksActionTypes.FETCH_TASKS_ERROR,
+                value: message
+            })
+        }
+    }
+
+}
