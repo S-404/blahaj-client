ad63ac870d111851a672f661b53da42524bece35
S-404 <sudarev.andrey.i@gmail.com>
2022-05-12 17:24:28 +0300

    init TaskHrefs.tsx

---
+++ src/components/todos/taskProperties/taskHrefs/TaskHrefsList.tsx
@@ -0,0 +1,19 @@
+import React, {FC} from 'react'
+import {ListGroup} from 'reactstrap'
+import TaskHrefsListItem from './TaskHrefsListItem'
+import {TaskHrefsPropsTypes} from '../../types/taskHrefsTypes'
+
+const TaskHrefsList:FC<TaskHrefsPropsTypes> = ({hrefs}) => {
+
+    if(!hrefs?.length) return <div>empty list</div>
+
+    return (
+        <ListGroup>
+            {hrefs.map(href=>(
+                <TaskHrefsListItem key={href.id} href={href}/>
+            ))}
+        </ListGroup>
+    )
+}
+
+export default TaskHrefsList
