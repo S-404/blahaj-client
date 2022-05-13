3ec8669d066e37970661d903d1e9667eea448f49
S-404 <sudarev.andrey.i@gmail.com>
2022-05-13 19:08:57 +0300

    init EditHrefForm.tsx

---
+++ src/components/todos/taskHrefs/EditHrefForm.tsx
@@ -0,0 +1,48 @@
+import React, {FC} from 'react'
+import {Button, Form} from 'reactstrap'
+import {useTypedSelector} from '../../../hooks/useTypedSelector'
+import {useActions} from '../../../hooks/useActions'
+import HrefInputs from './HrefInputs'
+import {taskHrefFormDataEntryValues} from '../helpers/taskHrefFormDataEntryValues'
+
+const EditHrefForm: FC = () => {
+
+    const {selectedTask, selectedTaskHref} = useTypedSelector(state => state.tasks)
+    const {updateHref,setEditTaskHrefModal} = useActions()
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        event.stopPropagation()
+
+        const {href, shortName, isMain} = taskHrefFormDataEntryValues(event)
+
+        updateHref({
+            id: selectedTaskHref.id,
+            teamId: selectedTask.teamId,
+            taskId: selectedTask.id,
+            href,
+            shortName,
+            isMain,
+        })
+
+        setEditTaskHrefModal(false)
+
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+
+            <HrefInputs {...selectedTaskHref}/>
+
+            <Button
+                block
+                color="success"
+                type="submit"
+            >
+                Apply
+            </Button>
+        </Form>
+    )
+}
+
+export default EditHrefForm
