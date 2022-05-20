470b1c929f862a08de486aa5f3d76369b32129bf
S-404 <sudarev.andrey.i@gmail.com>
2022-05-20 16:43:50 +0300

    fix api refresh token

--- src/api/index.ts
+++ src/api/index.ts
@@ -21,7 +21,7 @@ _api.interceptors.response.use((config) => {
     if (error.response.status === 401 && error.config && !error.config._isRetry) {
         originalRequest._isRetry = true;
         try {
-            const response = await axios.get<AuthResponse>(`${API_URL}/auth/refresh`)
+            const response = await axios.get<AuthResponse>(`${API_URL}/auth/refresh`,{withCredentials: true})
             localStorage.setItem('token', response.data.accessToken);
             return _api.request(originalRequest);
         } catch (e) {
